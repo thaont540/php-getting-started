fc43fab5a5cdf590b01bc929455f1ff2e54ab6c6
thaont-0210 <nguyen.the.thao@sun-asterisk.com>
2021-04-14 10:39:45 +0700

    Update all.php

--- web/all.php
+++ web/all.php
@@ -2,10 +2,8 @@
 header('Content-type: text/xml');
 ?>
 <Response>
-  <Dial>
-    <Conference>My conference</Conference>
-  </Dial>
   <Dial callerId="+15106940818">
+        <Conference>thao hihi</Conference>
         <Client><?php echo isset($_GET['client_name']) ? $_GET['client_name'] : 'the_user_id' ?></Client>
     </Dial>
 </Response>
