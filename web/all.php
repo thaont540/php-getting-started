46d20b719ca0f1d7d763ebd027c0eb877ab1f138
thaont-0210 <nguyen.the.thao@sun-asterisk.com>
2021-04-14 11:27:22 +0700

    Update all.php

--- web/all.php
+++ web/all.php
@@ -2,8 +2,8 @@
 header('Content-type: text/xml');
 ?>
 <Response>
-  <Dial callerId="+15106940818">
-        <Conference>thao hihi</Conference>
-        <Client><?php echo isset($_GET['client_name']) ? $_GET['client_name'] : 'the_user_id' ?></Client>
+    <Dial callerId="+15106940818">
+        <Client url='https://twilio-demo-call.herokuapp.com/conference.php'
+        ><?php echo isset($_GET['client_name']) ? $_GET['client_name'] : 'the_user_id' ?></Client>
     </Dial>
 </Response>
