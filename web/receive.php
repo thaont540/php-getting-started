40c809c8a13ed66f4891c339d38d735988381b6d
thaont-0210 <nguyen.the.thao@sun-asterisk.com>
2021-04-14 09:22:35 +0700

    Update receive.php

--- web/receive.php
+++ web/receive.php
@@ -2,7 +2,7 @@
 header('Content-type: text/xml');
 ?>
 <Response>
-  <Dial>
-    <Conference>My conference</Conference>
-  </Dial>
+    <Dial callerId="+15106940818">
+        <Client><?php echo isset($_GET['client_name']) ? $_GET['client_name'] : 'the_user_id' ?></Client>
+    </Dial>
 </Response>
