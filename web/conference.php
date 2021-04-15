ffcdc11b07215fe3179b789e5dd726e85863393d
thaont-0210 <nguyen.the.thao@sun-asterisk.com>
2021-04-15 09:52:03 +0700

    Update conference.php

--- web/conference.php
+++ web/conference.php
@@ -3,6 +3,6 @@ header('Content-type: text/xml');
 ?>
 <Response>
   <Dial>
-    <Conference statusCallbackEvent="initiated join" maxParticipants="2" statusCallback="https://call-mmp.sun-asterisk.vn/client-receive/54447290-774d-11eb-a31a-02420a0a0f05?client_name=the_user_id" startConferenceOnEnter="true">Thao hihi</Conference>
+    <Conference statusCallbackEvent="initiated join" statusCallback="https://call-mmp.sun-asterisk.vn/client-receive/54447290-774d-11eb-a31a-02420a0a0f05?client_name=the_user_id" startConferenceOnEnter="true">Thao hihi</Conference>
   </Dial>
 </Response>
